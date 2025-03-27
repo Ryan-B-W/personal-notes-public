913a8664f729235cb3e2383efbf19a5f1139b9a3
Ryan Wilcox <ryanbw@sdf.org>
2025-03-26 21:44:57 -0700

    Add Emacs .dir-locals.el to make repository an independent org-roam

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((eval . (setq-local org-directory (expand-file-name (locate-dominating-file default-directory ".dir-locals.el"))))
+         (eval . (setq-local org-id-locations-file (expand-file-name ".org-id-locations" org-directory)))
+         (eval . (setq-local org-roam-directory org-directory))
+         (eval . (setq-local org-roam-db-location (expand-file-name  ".org-roam.db" org-roam-directory))))))
